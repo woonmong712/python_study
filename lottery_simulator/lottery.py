9bbfd00ca3fc9f2b12c20233f9d1f7b2e67e8893
woonmong <kiata@naver.com>
2021-10-03 22:21:34 +0900

    complete

--- lottery_simulator/lottery.py
+++ lottery_simulator/lottery.py
@@ -12,35 +12,38 @@ def generate_numbers(n):
     return num_list
 
 def draw_winning_numbers():
-    num_list = generate_numbers(6)
-    sorted_num_list = sorted(num_list)
-    sorted_num_list.append(randint(1,45))
-    #print(f"draw winning numbers : {sorted_num_list}")
-    return sorted_num_list
+    winning_numbers = generate_numbers(7)
+    return sorted(winning_numbers[:6]) + winning_numbers[6:]
 
 def count_matching_numbers(numbers, winning_numbers):
     new_list = []
     for i in winning_numbers:
         if i in numbers:
             new_list.append(i)
-    #print(f"동일 숫자 : {new_list}")
     return new_list
+
 
 def check(numbers,winning_numbers):
     check_list = count_matching_numbers(numbers,winning_numbers)
     winning_price = 0
-    if len(check_list) == 6:
-        if winning_numbers[6] in check_list:
-            #print(f"보너스숫자 : {winning_numbers[6]}")
-            winning_price = 50000000
-        else:
+
+    if winning_numbers[6] not in check_list:
+        if len(check_list) == 6:
             winning_price = 100000000
-    elif len(check_list) == 5:
-        winning_price = 1000000
-    elif len(check_list) == 4:
-        winning_price = 50000
-    elif len(check_list) == 3:
-        winning_price = 5000
+        elif len(check_list) == 5:
+            winning_price = 1000000
+        elif len(check_list) == 4:
+            winning_price = 50000
+        elif len(check_list) == 3:
+            winning_price = 5000
+    elif winning_numbers[6] in check_list:
+        if len(check_list) == 6:
+            winning_price = 50000000
+        elif len(check_list) == 5:
+            winning_price = 50000
+        elif len(check_list) == 4:
+            winning_price = 5000
+
     return winning_price
     
 #print(f"금액 : {check(generate_numbers(NUMBER),draw_winning_numbers())} 원")
