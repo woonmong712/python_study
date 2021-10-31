eeaac875b791eb6eaadc60caa23b91e98bce9f50
woonmong <kiata@naver.com>
2021-10-31 21:45:22 +0900

    modify code

--- codeup/p1930.py
+++ codeup/p1930.py
@@ -1,24 +1,18 @@
-k,n = map(int,input().split(" "))
-result = 0
-result_memo = []
-
-
 def SuperSum(k,n):
+    sum = 0
     if k != 0:
-        print(f"k : {k} , n: {n}")
-        return SuperSum(k-1,n)
+        for i in range(1,n+1):
+            sum += SuperSum(k-1,i)
+        return sum
     elif k == 0:
-        print(f"k=0, n = {n}")
         return n
 
-for i in range(1,n+1):
-    print(f"range : {i}")
-    result_memo.append(SuperSum(k,i))
-    
-for i in result_memo:
-    result += i
 
-print(result_memo)
-print(result)
-    # SuperSum(0,n) = n 
-    # SuperSum(k,n) = SuperSum(k-1,0)+ ~~ + SuperSum(k-1,n)
+while True:
+    k,n = map(int,input().split(" "))
+    
+
+
+
+
+print(SuperSum(k,n))
