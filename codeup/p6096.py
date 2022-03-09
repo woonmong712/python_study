354b92ee70061696205351c1d6d7e0877fdd9a0c
SUNGJIN <kiata@naver.com>
2022-03-09 20:58:57 +0900

    modify(file name) and solve the p6096

--- codeup/p6096.py
+++ codeup/p6096.py
@@ -1,11 +1,22 @@
 # 2차원 배열 입력받기 
 arr = [list(map(int, input().split())) for _ in range(19)]
 n = int(input())
+
+
 for i in range(n):
     x,y = map(int,input().split(" "))
+    for j in range(0,19):
+        if arr[j][int(y-1)] == 0:
+            arr[j][int(y-1)] = 1
+        else:
+            arr[j][int(y-1)] = 0
+        if arr[int(x-1)][j] == 0:
+            arr[int(x-1)][j] = 1
+        else:
+            arr[int(x-1)][j] = 0
 
-for i in range(1,20):
-    if arr[i][int(y)] == 0:
-        arr[i][int(y)] = 1
-    else:
-        arr[i][int(y)] = 0
+# 출력하기
+for i in range(len(arr)):            # 세로 크기
+    for j in range(len(arr[i])):     # 가로 크기
+        print(arr[i][j], end=' ')
+    print()
