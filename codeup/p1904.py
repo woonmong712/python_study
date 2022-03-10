49c325b4e3544c928d9fa687c22089c028c06c88
woonmong <kiata@naver.com>
2022-03-10 22:27:54 +0900

    solve

--- codeup/p1904.py
+++ codeup/p1904.py
@@ -1,2 +1,12 @@
-a,b = int(input().split(" "))
+a,b = map(int,input().split(" "))
 
+def print_oddNum(a,b):
+    if a%2 != 0 and a <= b:
+        print(a)
+        a += 1
+        print_oddNum(a,b)
+    elif a%2 == 0 and a <= b:
+        a += 1
+        print_oddNum(a,b)
+
+print_oddNum(a,b)
