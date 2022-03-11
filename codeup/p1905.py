91a2868e2a03963a70e58b83030ef645958d4c8e
woonmong <kiata@naver.com>
2022-03-11 22:31:00 +0900

    풀이

--- codeup/p1905.py
+++ codeup/p1905.py
@@ -1,11 +1,12 @@
 n = int(input())
+total_result = 0
 
-def total_sum(n):
-    print(n)
-    n += n
-    if n != 0:
-        total_sum(n-1)
-    return n
+def total_sum(n,total_result):
+    total_result += n
+    if n > 0:
+        total_sum(n-1,total_result)
 
 
-print(total_sum(n))
+total_result = total_sum(n,total_result)
+
+print(total_result)
