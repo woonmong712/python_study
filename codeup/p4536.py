fa56e9e3574824af86ac30045b900d1d441c42f2
woonmong <kiata@naver.com>
2022-03-29 21:20:21 +0900

    commit

--- codeup/p4536.py
+++ codeup/p4536.py
@@ -1,7 +1,9 @@
+from collections import Counter
+
 numArr = []
 
 for i in range(10):
-    numArr.append(input())
+    numArr.append(int(input()))
 
 def Average_Num(numArr):
     sum = 0
@@ -9,15 +11,22 @@ def Average_Num(numArr):
         sum += int(numArr[i])
     return int(sum/len(numArr))
 
-def max_Cnt(numArr):
-    cnt = 0
-    dict = {numArr[i]: 0 for i in range(0, len(numArr))}
-    numArr2 = numArr
-    for i in range(len(numArr)):
-        for j in numArr2:
-            cnt += 1
-        
-    return dict
-    # print(dict)
+def find_max_num(array):
+    # count_num = {}
+    # for num in array:
+    #     if num not in count_num:
+    #         count_num[num] = 0
+    #     count_num[num] += 1
+    # count_num = sorted(count_num.items(), key=lambda x: x[1], reverse=True)
+    # return count_num[0][0]
+    counter = Counter(array)
+    max_count = -1
+    for letter in counter:
+        if counter[letter] > max_count:
+            max_count = counter[letter]
+            max_letter = letter
+    return max_letter
+    
+
 print(Average_Num(numArr))
-print(max_Cnt(numArr))
+print(find_max_num(numArr))
