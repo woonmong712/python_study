576777560966726f370aece197de3ad8fcaeb86c
SUNGJIN <kiata@naver.com>
2022-03-31 17:31:12 +0900

    commit

--- codeup/p4536.py
+++ codeup/p4536.py
@@ -11,13 +11,13 @@ def Average_Num(numArr):
 
 def max_Cnt(numArr):
     cnt = 0
-    dict = {numArr[i]: 0 for i in range(0, len(numArr))}
-    numArr2 = numArr
-    for i in range(len(numArr)):
-        for j in numArr2:
-            cnt += 1
-        
-    return dict
+    max_num = numArr[0]
+    for number in numArr:
+        for compare_number in number:
+            if number == compare_number:
+                cnt += 1
+                max_num = number
+            
     # print(dict)
 print(Average_Num(numArr))
 print(max_Cnt(numArr))
