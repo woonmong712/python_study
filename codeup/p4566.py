be939fbb118e309e48d9af7bad3df052ec27398d
woonmong <kiata@naver.com>
2022-04-19 12:08:06 +0900

    commit

--- codeup/p4566.py
+++ codeup/p4566.py
@@ -7,13 +7,25 @@ def create_Numlist(m,n):
         numlist.append(num)
     return numlist
 
-def is_Prime(m,n):
-    prime_array = []
-    numlist = create_Numlist(m,n)
-    for i in range(2,len(numlist)):
-        if numlist[i-2]%i != 0:
-            prime_array.append(numlist[i])
-    return prime_array
+def prime_list(m,n):
+    primeList = create_Numlist(m,n)
+    max = int(n**0.5)
+    for i in range(len(primeList)):
+        for j in range(2,max+1):
+            if primeList[i] % j == 0 and primeList[i] / j != 1 or primeList[i] == 1:
+                primeList[i] = 0
+    remove_set = {0}
+    primeList = [i for i in primeList if i not in remove_set]
+    return primeList
 
-print(create_Numlist(m,n))
-print(is_Prime(m,n))
+def total_sum(numList):
+    total = 0
+    for i in range(len(numList)):
+        total += numList[i]
+    return total
+
+primeList = []
+primeList = prime_list(m,n)
+
+print(total_sum(primeList))
+print(primeList[0])
