b2fdcb690bd07d5b3fc839e2a84cb0274b9736e1
woonmong <kiata@naver.com>
2022-05-23 12:32:36 +0900

    commit

--- codeup/p4571.py
+++ codeup/p4571.py
@@ -1,9 +1,13 @@
 m = int(input())
 n = int(input())
 
-numberList = []
-for i in range(n, m):
-    numberList.append(i)
+perfect_square_root = []
+for num in range(m,n+1):
+    if (num**(1/2)).is_integer() == True:
+        perfect_square_root.append(num)
 
-
-# def issquare(numberlist):
+if not perfect_square_root:
+    print("-1")
+else:
+    print(sum(perfect_square_root))
+    print(min(perfect_square_root))
