987cf42c76ae91af85cc0fda4cefd96d2c98c16e
woonmong <kiata@naver.com>
2022-06-07 13:01:22 +0900

    commit

--- codeup/p4626.py
+++ codeup/p4626.py
@@ -1,22 +1,14 @@
-from re import M
-
-
 cnt = int(input())
 answer = list(map(int, input().split()))
 
-my_grade = []
-grade = 0
-
-for i in range(len(answer)):
-    if answer[i] == 1:
-        grade = 1
-        if i != 0 :
-            my_grade.append(grade)
-        elif answer[i-1] == 1:
-            grade += 1
-            my_grade.append(grade)
-        elif answer[i-1] == 0:
-            grade = 0
-            my_grade.append(grade)
-
-print(my_grade)
+grade_array = []
+grade_count = 0
+for grade in answer:
+    if grade == 1:
+        grade_count += 1
+        grade_array.append(grade_count)
+    elif grade == 0:
+        grade_count = 0
+        grade_array.append(grade_count)   
+    
+print(sum(grade_array))
