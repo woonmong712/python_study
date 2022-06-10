c91ef81009dc03fb11e8bc2537ee7c16900e0f66
woonmong <kiata@naver.com>
2022-06-10 12:56:04 +0900

    commit

--- codeup/p4711.py
+++ codeup/p4711.py
@@ -1,1 +1,10 @@
-passenger = 
+train_passenger = []
+for num in range(4):
+    disembarking_passengers,passenger = map(int,input().split(" "))
+    train_passenger.append([disembarking_passengers,passenger])
+
+def current_Passenger(train_passenger):
+    
+    return 0
+
+print(train_passenger)
