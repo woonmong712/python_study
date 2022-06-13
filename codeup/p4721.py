6ad64589c2fa11a44b5284bc11835f47214923d8
woonmong <kiata@naver.com>
2022-06-13 13:00:43 +0900

    commit

--- codeup/p4721.py
+++ codeup/p4721.py
@@ -5,5 +5,13 @@ for i in range(student):
     a,b,c = map(int,input().split(" "))
     vote_grade.append([a,b,c])
 
+total_grade = {1:0,2:0,3:0}
+grade = 0
+for i in range(3):
+    for j in range(student):
+        grade += vote_grade[j][i]
+    total_grade[i+1] = grade
+    grade = 0
 
-print(vote_grade)
+arr = (k for k , v in total_grade.items() if max(total_grade.values) == v)
+print(arr)
