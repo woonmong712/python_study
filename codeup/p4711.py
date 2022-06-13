c96ab3cc98df8629d44349248698854e250401da
woonmong <kiata@naver.com>
2022-06-13 12:24:03 +0900

    push

--- codeup/p4711.py
+++ codeup/p4711.py
@@ -1,1 +1,15 @@
-passenger =
+train_passenger = []
+for num in range(4):
+    disembarking_passengers,passenger = map(int,input().split(" "))
+    train_passenger.append([disembarking_passengers,passenger])
+
+def current_Passenger(train_passenger):
+    passenger = 0
+    passengers = []
+    for i in range(4):
+        passenger += train_passenger[i][1] - train_passenger[i][0]
+        passengers.append(passenger)
+    current_Passenger_cnt = max(passengers)
+    return current_Passenger_cnt
+
+print(current_Passenger(train_passenger))
