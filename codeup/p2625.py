d73c70c115b266806b9270d73de857bc15ea36ca
woonmong <kiata@naver.com>
2022-08-16 12:05:24 +0900

    commit

--- codeup/p2625.py
+++ codeup/p2625.py
@@ -5,8 +5,8 @@ for a in range(n,0,-1):
     if n-a > a:
         for b in range(n-a,0,-1):
             c = n-a-b
-            if c != 0 and a >= b and a >= c:
+            if c != 0 and a >= b and b >= c:
                 triangle_array.append([a,b,c])
 
-triangle_array = set([tuple(sorted(item)) for item in triangle_array])
+# triangle_array = set([tuple(sorted(item)) for item in triangle_array])
 print(len(triangle_array))
--- codeup/p2626.py
+++ codeup/p2626.py
@@ -1,13 +1,19 @@
 n = int(input())
+count = 0
 
-triangle_array = []
+# # 가장 긴 변 A
+# for C in range(1,n//2+1):
+#     for A in range(0,(n-C)//2):
+#         print(A)
+#         B = n - (A+C)
+#         if (A+B) > C and C >= B and A <= B:
+#             count += 1
 
-for a in range(n,0,-1):
-    if n-a > a:
-        for b in range(n-a,0,-1):
-            c = n-a-b
-            if c != 0 and a >= b and a >= c:
-                triangle_array.append([a,b,c])
-
-triangle_array = set([tuple(sorted(item)) for item in triangle_array])
-print(len(triangle_array))
+for a in range(n//2,0,-1):
+    if a < (n-a):
+        for b in range(1,(n-a//2)):
+            if a >= b:
+                c = n-a-b
+                if c > 0 and b >= c:
+                    count += 1
+print(count)
