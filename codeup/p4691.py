54f5e6bd43f199c67cbfeff9ced6050f1d2c8d65
woonmong <kiata@naver.com>
2022-09-10 20:16:37 +0900

    commit (change dir path)

--- codeup/p4691.py
+++ codeup/p4691.py
@@ -14,11 +14,19 @@ def get_count(num_list):
 
 number_dict = []
 total_price = 0
+total_price_array = []
 for i in range(participant):
     number_dict.append(get_count(dice_number[i]))
 
 for i in number_dict:
     for key, value in i.items():
-        if value == 4:
-            total_price = 50000 +  int(key) * 5000
-    print(f"total_price = {total_price}")
+      if value == 4:
+        total_price = 50000 +  int(key) * 5000
+        total_price_array.append(total_price)
+        total_price = 0
+      elif value == 3:
+        total_price = 10000 + int(key) * 1000
+        total_price_array.append(total_price)
+        total_price = 0
+      print(key, value)
+    print(f"total_price = {total_price_array}")
