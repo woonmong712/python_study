1894b3f1d089d240fec2fb9d9b5de8db273bb4ae
woonmong <kiata@naver.com>
2023-07-25 01:08:54 +0900

    commit

---
+++ codeup/p3001~4000/p3709.py
@@ -0,0 +1,19 @@
+# 2*n 의 n 값 받기
+n = int(input())
+
+# 네모 상자를 채우는 공식 : T(i) = T(i-1) +T(i-2)
+def fibo(n):
+    cnt = 0
+    if n == 1:
+        cnt = 1
+    elif n == 2:
+        cnt = 2
+
+    fib = [1,2]
+    for i in range(2,n):
+        fib.append(fib[i-1]+fib[i-2])
+    cnt = fib[n-1]
+    return cnt
+
+result = fibo(n)%100000007
+print(result)
