e28700c6f62d79300eadadbe9187e280474d478d
woonmong <kiata@naver.com>
2023-07-29 18:16:09 +0900

    solve

--- codeup/p2001~3000/p2706.py
+++ codeup/p2001~3000/p2706.py
@@ -2,21 +2,16 @@ def find_max_sum(numbers):
     n = len(numbers)
     dp = [0] * (n + 1)
 
-    # 한 자리 수는 그대로 저장
     dp[1] = int(numbers[0])
-
+    
+    # 1의 자리로 나누는 경우와 2의자리로 나누는 경우의 max값을 찾아가도록 구현
     for i in range(2, n + 1):
-        # 현재 숫자를 1자리 수로 나누는 경우
-        dp[i] = max(dp[i], dp[i - 1] + int(numbers[i - 1]))
-
-        # 현재 숫자와 이전 숫자를 2자리 수로 묶는 경우
-        if i >= 2:
-            dp[i] = max(dp[i], dp[i - 2] + int(numbers[i - 2:i]))
+        dp[i] = max(dp[i - 1] + int(numbers[i - 1]), dp[i - 2] + int(''.join(numbers[i - 2:i]))) # 변환 시 오류가 나서 합쳐서 변환하는 것으로 변경
 
     return dp[n]
 
 # 입력 예시
-n = list(str(input()))
+numbers = list(input())
 
-result = find_max_sum(n)
-print(result)  # 출력 결과: 296
+result = find_max_sum(numbers)
+print(result)
