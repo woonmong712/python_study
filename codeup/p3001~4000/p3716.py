e49b4fc65c2e3786773feb74d79599dbac81a8ad
woonmong <kiata@naver.com>
2023-08-08 20:09:38 +0900

    풀이중

--- codeup/p3001~4000/p3716.py
+++ codeup/p3001~4000/p3716.py
@@ -1,1 +1,19 @@
-# 3*2*1 , 3*1*1
+n = int(input())
+
+def count_block_patterns(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+
+    dp = [0] * (n + 1)
+    dp[1] = 1
+    dp[2] = 2
+
+    for i in range(3, n + 1):
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 1000
+
+    return dp[n]
+
+result = count_block_patterns(n)
+print(result)
